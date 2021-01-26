35e4958a4b5c8655d4ca66ade57743947c2453cc
ebruaras <ebruaras.96@hotmail.com>
2021-01-26 15:28:03 +0300

    Revert "swagger"
    This reverts commit 85a1180ad93bb03a521853c83c7f0a4feb515209.

--- Hotel.WebUI/Controllers/HotelsController.cs
+++ Hotel.WebUI/Controllers/HotelsController.cs
@@ -18,49 +18,28 @@ namespace Hotel.WebUI.Controllers
         {
             _hotelService = hotelService;
         }
-        /// <summary>
-        /// Get All Hotels
-        /// </summary>
-        /// <returns></returns>
+
         [HttpGet]
         public List<Hotel.Entities.Hotel> GetAllHotels()
         {
             return _hotelService.GetAllHotels();
         }
-        /// <summary>
-        /// Get Hotel By Id
-        /// </summary>
-        /// <param name="id"></param>
-        /// <returns></returns>
         [HttpGet("{id}")]
         public Hotel.Entities.Hotel GetHotelById(int id)
         {
             return _hotelService.GetHotelById(id);
         }
-        /// <summary>
-        /// Create an Hotel
-        /// </summary>
-        /// <param name="hotel"></param>
-        /// <returns></returns>
         [HttpPost]
         public Hotel.Entities.Hotel PostHotel([FromBody]Hotel.Entities.Hotel hotel)
         {
             return _hotelService.CreateHotel(hotel);
         }
-        /// <summary>
-        /// Update the Hotel
-        /// </summary>
-        /// <param name="hotel"></param>
-        /// <returns></returns>
+
         [HttpPut]
         public Hotel.Entities.Hotel PutHotel([FromBody] Hotel.Entities.Hotel hotel)
         {
             return _hotelService.UpdateHotel(hotel);
         }
-        /// <summary>
-        /// Delete the Hotel
-        /// </summary>
-        /// <param name="id"></param>
         [HttpDelete("{id}")]
         public void DeleteHotel(int id)
         {
--- Hotel.WebUI/Startup.cs
+++ Hotel.WebUI/Startup.cs
@@ -32,18 +32,6 @@ namespace Hotel.WebUI
             //constructor da IHotelservice isteniyorsa HotelService üret
             services.AddSingleton<IHotelService, HotelService>();
             services.AddSingleton<IHotelRepository, HotelRepository>();
-            services.AddSwaggerDocument(config=> {
-                config.PostProcess = (doc => {
-                    doc.Info.Title = "Hotels API";
-                    doc.Info.Version = "1.0.13";
-                    doc.Info.Contact = new NSwag.OpenApiContact()
-                    {
-                        Name = "Ebru Aras",
-                        Email="ebru@hotmail.com"
-                    };
-               });
-
-            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -55,9 +43,6 @@ namespace Hotel.WebUI
             }
 
             app.UseRouting();
-
-            app.UseOpenApi();
-            app.UseSwaggerUi3();
 
             app.UseAuthorization();
 
